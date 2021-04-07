7a3980fb15f7e598f63a20823488f220da5dc18c
sonya <sonp2401@gmail.com>
2021-04-07 21:52:19 +0300

    Добавлена возможность строить маршруты, выбирая пункты пути из карточек

--- app/src/main/java/com/example/newmap/LocationRecyclerViewAdapter.java
+++ app/src/main/java/com/example/newmap/LocationRecyclerViewAdapter.java
@@ -1,14 +1,20 @@
 package com.example.newmap;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 
 import java.lang.ref.WeakReference;
@@ -16,18 +22,13 @@ import java.util.List;
 
 public class LocationRecyclerViewAdapter extends
         RecyclerView.Adapter<LocationRecyclerViewAdapter.MyViewHolder> {
-
-    private List<SingleRecyclerViewLocation> locationList;
-    private MapboxMap map;
-    private WeakReference<MainActivity> weakReference;
+    private List<Place> list;
+    Context context;
 
 
-    public LocationRecyclerViewAdapter(MainActivity activity,
-                                       List<SingleRecyclerViewLocation> locationList,
-                                       MapboxMap mapBoxMap) {
-        this.locationList = locationList;
-        this.map = mapBoxMap;
-        this.weakReference = new WeakReference<>(activity);
+    public LocationRecyclerViewAdapter(List<Place> list,Context context) {
+        this.list=list;
+        this.context=context;
     }
 
     @Override
@@ -39,49 +40,71 @@ public class LocationRecyclerViewAdapter extends
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        SingleRecyclerViewLocation singleRecyclerViewLocation = locationList.get(position);
-        holder.name.setText(singleRecyclerViewLocation.getName());
-        holder.numOfAvailableTables.setText(singleRecyclerViewLocation.getAvailableTables());
-        holder.setClickListener(new ItemClickListener() {
+        holder.name.setText(list.get(position).getTitle());
+        holder.imageView.setBackgroundResource(list.get(position).getImageCart());
+        holder.add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view, int position) {
-                weakReference.get()
-                        .drawNavigationPolylineRoute(weakReference.get().directionsRouteList.get(position));
+            public void onClick(View v) {
+                holder.add.setBackgroundColor(Color.GRAY);
+                holder.add.setText("Убрать из маршрута");
+                MainActivity.points.add(Point.fromLngLat(list.get(position).y,list.get(position).x));
+                LocationComponent locationComponent = MainActivity.mbMap.getLocationComponent();
+                Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                        locationComponent.getLastKnownLocation().getLatitude());
+                MainActivity.way(Point.fromLngLat(list.get(position).y,list.get(position).x),context);
+                MainActivity.points.add(Point.fromLngLat(list.get(position).y,list.get(position).x));
             }
         });
+//        SingleRecyclerViewLocation singleRecyclerViewLocation = locationList.get(position);
+//        holder.name.setText(singleRecyclerViewLocation.getName());
+//        holder.numOfAvailableTables.setText(singleRecyclerViewLocation.getAvailableTables());
+//        holder.setClickListener(new ItemClickListener() {
+//            @Override
+//            public void onClick(View view, int position) {
+////                weakReference.get()
+////                        .drawNavigationPolylineRoute(weakReference.get().directionsRouteList.get(position));
+//            }
+//        });
     }
 
     @Override
     public int getItemCount() {
-        return locationList.size();
+        return list.size();
     }
 
-    static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
         TextView name;
-        LinearLayout singleCard;
-        ItemClickListener clickListener;
-        TextView numOfAvailableTables;
+        Button add;
+//        LinearLayout singleCard;
+//        ItemClickListener clickListener;
+//        TextView numOfAvailableTables;
 
         MyViewHolder(View view) {
             super(view);
-            name = view.findViewById(R.id.location_title_tv);
-            singleCard = view.findViewById(R.id.single_location_cardview);
-            numOfAvailableTables = view.findViewById(R.id.location_num_of_beds_tv);
-            singleCard.setOnClickListener(this);
+            imageView = view.findViewById(R.id.prev);
+            name= view.findViewById(R.id.title);
+            add = view.findViewById(R.id.add);
+//            name = view.findViewById(R.id.location_title_tv);
+//            singleCard = view.findViewById(R.id.single_location_cardview);
+//            numOfAvailableTables = view.findViewById(R.id.location_num_of_beds_tv);
+//            singleCard.setOnClickListener(this);
         }
 
-        public void setClickListener(ItemClickListener itemClickListener) {
-            this.clickListener = itemClickListener;
+//        public void setClickListener(ItemClickListener itemClickListener) {
+//            this.clickListener = itemClickListener;
+//        }
+
+//        @Override
+//        public void onClick(View view) {
+//            clickListener.onClick(view, getLayoutPosition());
+//        }
+//    }
+
+        public interface ItemClickListener {
+            void onClick(View view, int position);
         }
 
-        @Override
-        public void onClick(View view) {
-            clickListener.onClick(view, getLayoutPosition());
-        }
-    }
-
-    public interface ItemClickListener {
-        void onClick(View view, int position);
     }
 
 }
--- app/src/main/java/com/example/newmap/MainActivity.java
+++ app/src/main/java/com/example/newmap/MainActivity.java
@@ -21,6 +21,7 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -30,6 +31,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mapbox.android.core.permissions.PermissionsListener;
@@ -83,48 +85,35 @@ import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher;
 
 public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapClickListener {
     private static final String TAG = "DirectionsActivity";
-    private static final String SYMBOL_ICON_ID = "SYMBOL_ICON_ID";
-    private static final String PERSON_ICON_ID = "PERSON_ICON_ID";
-    private static final String MARKER_SOURCE_ID = "MARKER_SOURCE_ID";
-    private static final String PERSON_SOURCE_ID = "PERSON_SOURCE_ID";
-    private static final String DASHED_DIRECTIONS_LINE_LAYER_SOURCE_ID = "DASHED_DIRECTIONS_LINE_LAYER_SOURCE_ID";
-    private static final String LAYER_ID = "LAYER_ID";
-    private static final String PERSON_LAYER_ID = "PERSON_LAYER_ID";
-    private static final String DASHED_DIRECTIONS_LINE_LAYER_ID = "DASHED_DIRECTIONS_LINE_LAYER_ID";
-
-    private static final Point directionsOriginPoint = Point.fromLngLat(100.48730850219725,
-            13.737217333153827);
-    private static final  LatLng[] possibleDestinations = new LatLng[]{
-            new LatLng(13.773399508046145, 100.51116943359375),
-            new LatLng(13.743387039520751, 100.45074462890625),
-            new LatLng(13.732715012486663, 100.5523681640625),
-            new LatLng(13.665336643848484, 100.45486450195312),
-            new LatLng(13.7153719325982, 100.49263000488281),
-            new LatLng(13.742053062720384, 100.51288604736328),
-            new LatLng(13.77773432408578, 100.4806137084961),
-            new LatLng(13.784736549340208, 100.55580139160156),
-            new LatLng(13.71670606117596, 100.45520782470703)
-    };
 
     public final List<DirectionsRoute> directionsRouteList = new ArrayList<>();
 
-    public MapView mapView;
-    public MapboxMap mbMap;
+    public static MapView mapView;
+    public static MapboxMap mbMap;
 
-    private Button buttonNavigation;
+    private static Button buttonNavigation;
+    Dialog dialog;
 
     public PermissionsManager permissionsManager;
-    private DirectionsRoute currentRoute;
-    private NavigationMapRoute navigationMapRoute;
-    private FeatureCollection dashedLineDirectionsFeatureCollection;
+    private static DirectionsRoute currentRoute;
+    private static NavigationMapRoute navigationMapRoute;
+    RecyclerView recyclerView;
+
+    public static List<Point> points = new ArrayList<>();
+    public List<Place> places = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.rv_on_top_of_map);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Начать путешесвие?");
+        dialog.setContentView(R.layout.dialog_view);
 
         mapView.getMapAsync(new OnMapReadyCallback() {
             @SuppressLint("ResourceAsColor")
@@ -133,110 +122,29 @@ public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapCl
                 mbMap = mapboxMap;
                 addMarkers();
 
+                LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this, RecyclerView.HORIZONTAL, false);
+                recyclerView.setLayoutManager(layoutManager);
+                LocationRecyclerViewAdapter adapter = new LocationRecyclerViewAdapter(places,MainActivity.this);
+                recyclerView.setAdapter(adapter);
+
+
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-                        localization(mbMap,style);
+                        localization(mbMap, style);
 
                         addDestinationIconSymbolLayer(style);
                         enableLocationComponent(style);
                         initMapStuff(style);
 
-                        mapboxMap.addOnMapClickListener(MainActivity.this);
+//                        mapboxMap.addOnMapClickListener(MainActivity.this);
 
                         startWay();
-
-                        getRoutesToAllPoints();
-                        initRecyclerView();
                     }
                 });
             }
         });
-    }
 
-    private void initRecyclerView() {
-        RecyclerView recyclerView = findViewById(R.id.rv_on_top_of_map);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(),
-                LinearLayoutManager.HORIZONTAL, true));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(new LocationRecyclerViewAdapter(this,
-                createRecyclerViewLocations(), mbMap));
-        new LinearSnapHelper().attachToRecyclerView(recyclerView);
-    }
-
-    private List<SingleRecyclerViewLocation> createRecyclerViewLocations() {
-        ArrayList<SingleRecyclerViewLocation> locationList = new ArrayList<>();
-        for (int x = 0; x < possibleDestinations.length; x++) {
-            SingleRecyclerViewLocation singleLocation = new SingleRecyclerViewLocation();
-            singleLocation.setName(String.format("Миса", x));
-            singleLocation.setAvailableTables(String.format("ошка",
-                    new Random().nextInt(possibleDestinations.length)));
-            locationList.add(singleLocation);
-        }
-        return locationList;
-    }
-
-    private void getRoutesToAllPoints() {
-        for (LatLng singleLatLng : possibleDestinations) {
-            getRoute2(Point.fromLngLat(singleLatLng.getLongitude(), singleLatLng.getLatitude()));
-        }
-    }
-
-    @SuppressWarnings({"MissingPermission"})
-    private void getRoute2(Point destination) {
-        MapboxDirections client = MapboxDirections.builder()
-                .origin(directionsOriginPoint)
-                .destination(destination)
-                .overview(DirectionsCriteria.OVERVIEW_FULL)
-                .profile(DirectionsCriteria.PROFILE_DRIVING)
-                .accessToken(getString(R.string.mapbox_access_token))
-                .build();
-        client.enqueueCall(new Callback<DirectionsResponse>() {
-            @Override
-            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
-                if (response.body() == null) {
-                    Log.d(TAG, "No routes found, make sure you set the right user and access token.");
-                    return;
-                } else if (response.body().routes().size() < 1) {
-                    Log.d(TAG, "No routes found");
-                    return;
-                }
-// Add the route to the list.
-                directionsRouteList.add(response.body().routes().get(0));
-            }
-
-            @Override
-            public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
-                Log.d(TAG, "Error: " + throwable.getMessage());
-                if (!throwable.getMessage().equals("Coordinate is invalid: 0,0")) {
-                    Toast.makeText(MainActivity.this,
-                            "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    public void drawNavigationPolylineRoute(final DirectionsRoute route) {
-        if (mbMap != null) {
-            mbMap.getStyle(new Style.OnStyleLoaded() {
-                @Override
-                public void onStyleLoaded(@NonNull Style style) {
-                    List<Feature> directionsRouteFeatureList = new ArrayList<>();
-                    LineString lineString = LineString.fromPolyline(route.geometry(), PRECISION_6);
-                    List<Point> lineStringCoordinates = lineString.coordinates();
-                    for (int i = 0; i < lineStringCoordinates.size(); i++) {
-                        directionsRouteFeatureList.add(Feature.fromGeometry(
-                                LineString.fromLngLats(lineStringCoordinates)));
-                    }
-                    dashedLineDirectionsFeatureCollection =
-                            FeatureCollection.fromFeatures(directionsRouteFeatureList);
-                    GeoJsonSource source = style.getSourceAs(DASHED_DIRECTIONS_LINE_LAYER_SOURCE_ID);
-                    if (source != null) {
-                        source.setGeoJson(dashedLineDirectionsFeatureCollection);
-                    }
-                }
-            });
-        }
     }
 
     //Добавление стилей для маркера по касанию
@@ -252,18 +160,6 @@ public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapCl
                 iconIgnorePlacement(true)
         );
         loadedMapStyle.addLayer(destinationSymbolLayer);
-    }
-
-    //генерация ресурсов для кастомизации маркеров
-    public static Icon drawableToIcon(@NonNull Context context, @DrawableRes int id, @ColorInt int colorRes) {
-        Drawable vectorDrawable = ResourcesCompat.getDrawable(context.getResources(), id, context.getTheme());
-        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
-                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-//        DrawableCompat.setTint(vectorDrawable, colorRes);
-        vectorDrawable.draw(canvas);
-        return IconFactory.getInstance(context).fromBitmap(bitmap);
     }
 
     //Обработка кнопки для возврата к местонахождению пользователя
@@ -331,21 +227,35 @@ public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapCl
             source.setGeoJson(Feature.fromGeometry(destinationPoint));
         }
 
-        getRoute(originPoint, destinationPoint);
+        getRoute(originPoint, destinationPoint,this);
 
         buttonNavigation.setEnabled(true);
         buttonNavigation.setBackgroundResource(R.color.purple_200);
-
         return true;
     }
 
+    public static void way(Point point,Context context){
+        LocationComponent locationComponent = mbMap.getLocationComponent();
+        Point originPoint = Point.fromLngLat(locationComponent.getLastKnownLocation().getLongitude(),
+                locationComponent.getLastKnownLocation().getLatitude());
+
+        getRoute(originPoint,point,context);
+
+        buttonNavigation.setEnabled(true);
+        buttonNavigation.setBackgroundResource(R.color.purple_200);
+    }
+
     //Построение пути от пользователя до маркера
-    private void getRoute(Point origin, Point destination) {
-        NavigationRoute.builder(this)
+    private static void getRoute(Point origin, Point destination,Context context) {
+        NavigationRoute.Builder builder = NavigationRoute.builder(context)
                 .accessToken(Mapbox.getAccessToken())
                 .origin(origin)
                 .destination(destination)
-                .build()
+                .profile(DirectionsCriteria.PROFILE_DRIVING);
+        for (int i = 0; i < points.size(); i++) {
+            builder.addWaypoint(points.get(i));
+        }
+        builder.build()
                 .getRoute(new Callback<DirectionsResponse>() {
                     @Override
                     public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
@@ -374,29 +284,20 @@ public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapCl
                         Log.e(TAG, "Error: " + throwable.getMessage());
                     }
                 });
+
     }
 
     //Добавление статичных маркеров на карту
     @SuppressLint("ResourceAsColor")
-    public void addMarkers(){
-        mbMap.addMarker(new MarkerOptions()
-                .position(new LatLng(52.596357, 38.924705))
-                .title("Галичья гора")
-                .setIcon(drawableToIcon(getBaseContext(), R.drawable.ic_baseline_place_24, R.color.black))
-                .snippet("Заповедник в Задонском районе"));
-
-        mbMap.addMarker(new MarkerOptions()
-                .position(new LatLng(52.959332, 38.924705))
-                .title("Кураповские скалы")
-                .snippet("Заповедник в Задонском районе"));
-
-        mbMap.addMarker(new MarkerOptions()
-                .position(new LatLng(52.573145, 38.352376))
-                .title("Воргольские скалы")
-                .snippet("Уникальное место для семейного отдыха и экстремального спорта"));
+    public void addMarkers() {
+//        points.add(Point.fromLngLat(52.596357, 38.924705));
+        places.add(new Place(52.596357, 38.924705, "Кудыкина гора", "Популярный сафари-парк, расположенный на берегу Дона", R.drawable.image2));
+        places.add(new Place(52.573145, 38.352376, "Кураповские скалы", "Заповедник в Задонском районе", R.drawable.image3));
+        places.add(new Place(52.959332, 39.029328, "Воргольские скалы", "Уникальное место для семейного отдыха и экстремального спорта", R.drawable.image1));
     }
 
-    public void localization(MapboxMap mapboxMap,Style style){
+    //Локализация
+    public void localization(MapboxMap mapboxMap, Style style) {
         LocalizationPlugin localizationPlugin = new LocalizationPlugin(mapView, mapboxMap, style);
         try {
             localizationPlugin.matchMapLanguageWithDeviceDefault();
@@ -405,19 +306,25 @@ public class MainActivity extends AppCompatActivity implements MapboxMap.OnMapCl
     }
 
     //Начала пути по навигатору
-    public void startWay(){
+    public void startWay() {
         buttonNavigation = findViewById(R.id.startButton);
         buttonNavigation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean simulateRoute = true;
+                Button button=dialog.findViewById(R.id.b);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        NavigationLauncherOptions options = NavigationLauncherOptions.builder()
+                                .directionsRoute(currentRoute)
+                                .shouldSimulateRoute(simulateRoute)
+                                .build();
 
-                NavigationLauncherOptions options = NavigationLauncherOptions.builder()
-                        .directionsRoute(currentRoute)
-                        .shouldSimulateRoute(simulateRoute)
-                        .build();
-
-                NavigationLauncher.startNavigation(MainActivity.this, options);
+                        NavigationLauncher.startNavigation(MainActivity.this, options);
+                    }
+                });
+                dialog.show();
             }
         });
     }
